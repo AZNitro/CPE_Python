3b247eb5f6c78d1cc694138f7e26628854f8358f
AZNitro <fivepeok05@outlook.com>
2026-03-04 16:22:42 +0800

    add 10222 - Decode the Mad man

---
+++ 10222 - Decode the Mad man.py
@@ -0,0 +1,22 @@
+def solve(text):
+    original = "234567890-=ertyuiop[]\dfghjkl;'cvbnm,./"
+
+    translated="`1234567890qwertyuiop[asdfghjklzxcvbnm,"
+
+    decode_table = str.maketrans(original, translated)
+
+    ans = text.translate(decode_table)
+
+    print(ans)
+
+
+while True:
+    try:
+        n = input()
+    except EOFError:
+        break
+
+    solve(n)
+
+#解題邏輯：1.建立好table對照 2.採用str.maketrans 和 translate 兩大關鍵
+#soloution logic: 1. create a table for decoding 2. implement with str.maketrans and translate these two key points.
